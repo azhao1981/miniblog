020bef5a249bda993e5afd7032dc25c1e7b7ee7f
weizhao <azhao.1981@gmail.com>
2012-02-28 23:17:31 +0800

    init and juery-ui-bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miniblog::Application.config.secret_token = 'bd92d11c7adb20631bc7a2f2404eee9e820ac24a1d6c188032365b87344a3373f9b96e04a65ac148f3f88a912407e06e1212414db8101c96f27722851493dd65'
